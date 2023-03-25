cc92629dc5f282bde22c2de070f3927119e5f724
not_joon <lbj199874@gmail.com>
2023-03-25 20:53:43 +0900

    update loop/1 function

--- actor/lib/actor.ex
+++ actor/lib/actor.ex
@@ -1,43 +1,52 @@
 defmodule Actor do
-  defstruct state: 0, mailbox: [], subscribers: MapSet.new()
+  defstruct state: 0, mailbox: :queue.new(), subscribers: MapSet.new()
 
-  # generate new actor
   @spec new(state :: integer) :: pid
-  def new(state \\ 0) do
-    spawn_link(__MODULE__, :loop, [state])
+  def new(state) do
+    spawn_link(__MODULE__, :loop, [%{state: state, mailbox: :queue.new(), subscribers: MapSet.new()}])
   end
 
   @spec loop(state :: integer) :: no_return
   def loop(state) do
     receive do
       {:get, pid} ->
-        send(pid, state)
-        loop(state)
+        send(pid, %{state | state: state[:state]})
+        loop(%{state | state: state})
 
       {:set, pid, new_state} ->
         send(pid, new_state)
-        loop(new_state)
+        loop(%{state | state: new_state})
 
-      {:kill, pid} ->
-        send(pid, :killed)
-        kill(pid)
+      {:subscribe, pid} ->
+        subscribers = MapSet.put(state[:subscribers], pid)
+        loop(%{state | subscribers: subscribers})
 
-      # TODO
-      # {:subscribe, pid} ->
-      #   subscribers = subscribe(self(), pid)
-      #   loop(%{state | subscribers: subscribers})
+      {:unsubscribe, pid} ->
+        subscribers = MapSet.delete(state[:subscribers], pid)
+        loop(%{state | subscribers: subscribers})
 
-      # {:unsubscribe, pid} ->
-      #   subscribers = unsubscribe(self(), pid)
-      #   loop(%{state | subscribers: subscribers})
+      {:message, value} ->
+        if MapSet.size(state[:subscribers]) == 0 do
+          message = :queue.in(value, state[:mailbox])
+          loop(%{state | mailbox: message})
+        else
+          Enum.each(state[:subscribers], fn pid ->
+            send(pid, value)
+          end)
+          loop(state)
+        end
+
+      _ ->
+        loop(state)
     end
   end
 
   @spec get(pid :: pid) :: integer
   def get(pid) do
     pid |> send({:get, self()})
+
     receive do
-      state -> state
+      state -> state[:state]
     end
   end
 
@@ -45,19 +54,13 @@ defmodule Actor do
   def set(pid, new_state) do
     pid |> send({:set, self(), new_state})
 
-    if get(pid) == :killed do
-      raise "Actor is killed"
-    end
-
     receive do
       state -> state
     end
   end
 
-  def kill(pid) do
-    if get(pid) == :killed do
-      raise "Actor has killed before"
-    end
-    pid |> send({:kill, self()})
+  def subscribers(pid) do
+    state = Process.get(pid, :state)
+    state[:subscribers]
   end
 end
